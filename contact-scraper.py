1db6a6f80b379ee33945d26bc6ef5e42e52ce0af
fortin <fortin@users.noreply.github.com>
2019-04-22 16:47:49 +0200

    feat: Phone number extraction
    Match works but stuck in infinite loop; when updating to `numbers()` set, it gives a `TypeError`.

--- contact-scraper.py
+++ contact-scraper.py
@@ -7,13 +7,35 @@
 #
 
 import re
+from talon.signature.bruteforce import extract_signature
+import postal_address
+import phonenumbers
 
 pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)")
 
 emails = set()
+numbers = set()
 
-for i, line in enumerate(open('test_email.eml')):
+email_file = open('test_email.eml')
+my_text = email_file.readlines()
+numbers_file = open('phonenumbers.txt')
+my_numbers = numbers_file.read()
+
+for i, line in enumerate(my_text):
     for match in re.finditer(pattern, line):
         emails.update(match.groups())
+    # try:
+    #     for match in phonenumbers.PhoneNumberMatcher(my_text, "US"):
+    #         phonenumbers.update(match.groups())
+    # except(TypeError):
+    #     print('Type Error')
 
-print(emails)
+for i, line in enumerate(my_numbers):
+    try:
+        for match in phonenumbers.PhoneNumberMatcher(my_numbers, "US"):
+            numbers.update(match.groups())
+            # print(match)
+    except(TypeError):
+        print('Type Error')
+
+print(emails, numbers)
