4d20d9a044ab1b8c74cd07106a3cddf62313c86f
fortin <fortin@users.noreply.github.com>
2019-04-24 11:55:54 +0200

    feat: phonenumbers working for US numbers
    Deleted email stuff for hygiene

--- contact-scraper.py
+++ contact-scraper.py
@@ -6,36 +6,17 @@
 #  Copyright (c) 2019 Antonio Fortin. All rights reserved.
 #
 
-import re
-from talon.signature.bruteforce import extract_signature
-import postal_address
 import phonenumbers
 
-pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)")
-
-emails = set()
-numbers = set()
-
-email_file = open('test_email.eml')
-my_text = email_file.readlines()
-numbers_file = open('phonenumbers.txt')
+numbers_file = open('test_email.eml', 'r')
 my_numbers = numbers_file.read()
 
-for i, line in enumerate(my_text):
-    for match in re.finditer(pattern, line):
-        emails.update(match.groups())
-    # try:
-    #     for match in phonenumbers.PhoneNumberMatcher(my_text, "US"):
-    #         phonenumbers.update(match.groups())
-    # except(TypeError):
-    #     print('Type Error')
+numbers = set()
 
-for i, line in enumerate(my_numbers):
+for match in phonenumbers.PhoneNumberMatcher(my_numbers, "US"):
     try:
-        for match in phonenumbers.PhoneNumberMatcher(my_numbers, "US"):
-            numbers.update(match.groups())
-            # print(match)
+        numbers.add(phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164))
     except(TypeError):
-        print('Type Error')
+        pass
 
-print(emails, numbers)
+print(numbers)
