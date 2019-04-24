bf491b4f11a23e0f02bfd0dcb59d1bfca2ee06fa
fortin <fortin@users.noreply.github.com>
2019-04-24 14:36:42 +0200

    refactor: Deleted extraneous lines

--- contact-scraper.py
+++ contact-scraper.py
@@ -14,8 +14,4 @@ addresses = set()
 for match in usaddress.parse(enumerate(open('test_email.eml'))):
     addresses.update(match.groups())
 
-# for i, line in enumerate(open('test_email.eml')):
-#     for match in re.finditer(pattern, line):
-#         addresses.update(match.groups())
-
 print(addresses)
