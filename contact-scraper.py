808b8aa268dc148223655f4b65ff2eeefc3c0777
fortin <fortin@users.noreply.github.com>
2019-04-24 14:41:06 +0200

    refactor: added master

--- contact-scraper.py
+++ contact-scraper.py
@@ -9,9 +9,16 @@
 import usaddress
 import re
 
+pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)")
+
+emails = set()
 addresses = set()
+
+for i, line in enumerate(open('test_email.eml')):
+    for match in re.finditer(pattern, line):
+        emails.update(match.groups())
 
 for match in usaddress.parse(enumerate(open('test_email.eml'))):
     addresses.update(match.groups())
 
-print(addresses)
+print(emails, addresses)
