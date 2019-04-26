9c98ae65cedd6872f2111f958340b7cdcb3bd2c6
fortin <fortin@users.noreply.github.com>
2019-04-26 20:19:50 +0200

    feat: headers2dict working
    Extracts headers from an email and converts them to a string.
    TODO: convert str to dict with json (bug due to rogue comma near end)
    TODO: extract From: and pass to contact dictionary with keys `{First Name: [], Last Name: [], email: []`

--- headers-test.py
+++ headers-test.py
@@ -5,22 +5,25 @@ import csv
 def headers2dict(filename):
     with open(filename, 'r') as f:
         headers = f.read()
-    headers = headers.replace("[(", "{\n", 1)
-    headers = headers.replace(")]", "\n}")
+    headers = headers.replace("[(", "{", 1)
+    headers = headers.replace(")]", "}")
     headers = headers.replace("',", "':")
     headers = re.sub(r"{(.+?}).+", "\\1", headers)
-    headers = headers.replace("), (", ", \n")
+    headers = headers.replace("'", '"')
+    headers = headers.replace("), (", ",")
     for char in headers:
         headers = headers.translate({ord(i):None for i in '[]'})
     return(headers)
 
-headers = headers2dict('test-headers.txt')
-
-wanted = ['From'] # The keys you want
-dict((k, headers[k]) for k in wanted if k in headers)
+headers = str(headers2dict('test-headers.txt'))
+print(headers)
+# headers = json.loads(headers)
+print(type(headers))
+# wanted = ['From'] # The keys you want
+# dict((k, headers[k]) for k in wanted if k in headers)
 # {from: headers[from] for from in headers.keys() & {'From'}}
 
-print(dict)
+# print(dict)
 # print(headers)
 # print(type(wanted))
 # print(dictionary)
