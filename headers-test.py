af3857c7815588bee6da8ed920336e3e8932da2e
fortin <fortin@users.noreply.github.com>
2019-04-26 20:19:50 +0200

    feat: headers2dict working
    Extracts headers from an email and converts them to a dictionary with keys and values
    TODO: extract From: and pass to contact dictionary with keys `{First Name: [], Last Name: [], email: []`

--- headers-test.py
+++ headers-test.py
@@ -2,19 +2,27 @@ import re
 import string
 import csv
 
-with open('test-headers.txt', 'r') as f:
-    headers = f.read()
+def headers2dict(filename):
+    with open(filename, 'r') as f:
+        headers = f.read()
+    headers = headers.replace("[(", "{\n", 1)
+    headers = headers.replace(")]", "\n}")
+    headers = headers.replace("',", "':")
+    headers = re.sub(r"{(.+?}).+", "\\1", headers)
+    headers = headers.replace("), (", ", \n")
+    for char in headers:
+        headers = headers.translate({ord(i):None for i in '[]'})
+    return(headers)
 
-headers = headers.replace("[", "{", 1)
-headers = headers.replace(")]", "}")
-headers = headers.replace("',", "':")
-headers = re.replace(s')
-print(headers)
-for char in headers:
-    headers = headers.translate({ord(i):None for i in '[]'})
+headers = headers2dict('test-headers.txt')
 
+wanted = ['From'] # The keys you want
+dict((k, headers[k]) for k in wanted if k in headers)
+# {from: headers[from] for from in headers.keys() & {'From'}}
 
-print(type(headers))
+print(dict)
+# print(headers)
+# print(type(wanted))
 # print(dictionary)
 #
 # names = ()
