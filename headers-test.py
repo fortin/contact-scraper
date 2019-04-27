013aa62beff1b1da8ff1f4cc3a5aba86cfca79db
fortin <fortin@users.noreply.github.com>
2019-04-27 15:39:02 +0200

    feat: Extracts headers and From:

--- headers-test.py
+++ headers-test.py
@@ -3,24 +3,18 @@ import string
 import csv
 import json
 
-def headers2dict(filename):
+def extract_headers(filename):
     with open(filename, 'r') as f:
         headers = f.read()
-    headers = headers.replace('"', '') # strip double quotes
-    headers = headers.replace("[(", "{", 1) # [( -> {
-    headers = headers.replace(")]", "}") # )] -> }
-    headers = headers.replace("',", "':") # ', -> ':
+    headers = headers.replace('"', '').replace("[(", "{", 1).replace(")]", "}").replace("',", "':").replace("'", '"').replace("), (", ",")
+    # strip double quotes, convert brackets [()] to braces and make dict-like string
     headers = re.sub(r"{(.+?}).+", "\\1", headers) # get rid of everything after the headers
-    headers = headers.replace("'", '"') # single to double quotes for dict
-    headers = headers.replace("), (", ",") # ), ( -> ,
-    for char in headers:
-        headers = headers.translate({ord(i):None for i in '[]'})
     return(headers)
 
 # TODO is there a less bruteforce way of doing headers2dict?
 
-headers = str(headers2dict('test-headers.txt'))
+headers = str(extract_headers('test-headers.txt'))
 headers = json.loads(headers)
-wanted = ['From', 'To'] # The keys you want
-from_value = dict((k, headers[k]) for k in wanted if k in headers)
+wanted = ['From'] # The keys you want
+from_value = dict((k, headers[k]) for k in ['From'] if k in headers)
 print(from_value)
