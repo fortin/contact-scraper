d7d2d13b5e0b59ee87c5816a1840e15ee13b7c8f
fortin <fortin@users.noreply.github.com>
2019-04-27 15:42:07 +0200

    feat: Extracts headers and From

--- contact-scraper.py
+++ contact-scraper.py
@@ -10,6 +10,25 @@ import phonenumbers
 import re
 import email
 import email.parser
+import string
+import csv
+import json
+
+def extract_headers(filename):
+    with open(filename, 'r') as f:
+        headers = f.read()
+    headers = headers.replace('"', '').replace("[(", "{", 1).replace(")]", "}").replace("',", "':").replace("'", '"').replace("), (", ",")
+    # strip double quotes, convert brackets [()] to braces and make dict-like string
+    headers = re.sub(r"{(.+?}).+", "\\1", headers) # get rid of everything after the headers
+    return(headers)
+
+# TODO is there a less bruteforce way of doing headers2dict?
+
+headers = str(extract_headers('test-headers.txt'))
+headers = json.loads(headers)
+wanted = ['From'] # The keys you want
+from_value = dict((k, headers[k]) for k in ['From'] if k in headers)
+print(from_value)
 
 pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)")
 msg = open('test_email.eml')
