abdf88997d01dcc647271cee4576213438ebb3cb
fortin <fortin@users.noreply.github.com>
2019-05-05 01:22:51 +0200

    refactor: Ran Atom Beautify

--- contact-scraper.py
+++ contact-scraper.py
@@ -10,6 +10,7 @@ import re
 from email.parser import HeaderParser
 import phonenumbers
 
+
 def header_parser(filename):
     # parse email headers
     with open(filename, 'r') as msg:
@@ -19,19 +20,22 @@ def header_parser(filename):
         headers = dict(h.items())
     return(headers)
 
+
 def num_catcher(filename):
     # extract all US phone numbers from message
     with open(filename, 'r') as msg:
         my_email = msg.read()
         numbers = set()
         for match in phonenumbers.PhoneNumberMatcher(my_email, "US"):
-            numbers.add(phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164))
-            if numbers != set(): # stop after first match
+            numbers.add(phonenumbers.format_number(
+                match.number, phonenumbers.PhoneNumberFormat.E164))
+            if numbers != set():  # stop after first match
                 break
         if numbers == set():
             numbers = None
         numbers = str(numbers).replace("{'", "").replace("'}", "")
     return(numbers)
+
 
 def email_catcher(filename):
     # extract all email addresses from message
@@ -43,45 +47,48 @@ def email_catcher(filename):
                 emails.update(match.groups())
     return(emails)
 
+
 def dict_pop(contact, numbers):
     if contact[0].isupper():
         contact = {
-                    'lastname' : contact[0].replace("[", "").replace("]", "").replace("'", ""),
-                    'firstname': contact[1].replace("[", "").replace("]", "").replace("'", ""),
-                    'email'    : contact[-1].replace("[", "").replace("]", "").replace("'", ""),
-                    'company'  : None,
-                    'address'  : None,
-                    'phone'    : numbers
-                    }
+            'lastname': contact[0].replace("[", "").replace("]", "").replace("'", ""),
+            'firstname': contact[1].replace("[", "").replace("]", "").replace("'", ""),
+            'email': contact[-1].replace("[", "").replace("]", "").replace("'", ""),
+            'company': None,
+            'address': None,
+            'phone': numbers
+        }
     elif len(contact) < 4:
         try:
             contact = {
-                        'lastname' : contact[1].replace("[", "").replace("]", "").replace("'", ""),
-                        'firstname': contact[0].replace("[", "").replace("]", "").replace("'", ""),
-                        'email'    : contact[-1].replace("[", "").replace("]", "").replace("'", ""),
-                        'company'  : None,
-                        'address'  : None,
-                        'phone'    : numbers
-                        }
+                'lastname': contact[1].replace("[", "").replace("]", "").replace("'", ""),
+                'firstname': contact[0].replace("[", "").replace("]", "").replace("'", ""),
+                'email': contact[-1].replace("[", "").replace("]", "").replace("'", ""),
+                'company': None,
+                'address': None,
+                'phone': numbers
+            }
         except:
             pass
     else:
-        company = str(contact[0:-1]).replace("[", "").replace("]", "").replace("'", "").replace('"', "").replace(",", "")
+        company = str(contact[0:-1]).replace("[", "").replace("]",
+                                                              "").replace("'", "").replace('"', "").replace(",", "")
         contact = {
-                    'lastname' : None,
-                    'firstname': None,
-                    'email'    : contact[-1].replace("[", "").replace("]", "").replace("'", ""),
-                    'company'  : company,
-                    'address'  : None,
-                    'phone'    : numbers
-                    }
+            'lastname': None,
+            'firstname': None,
+            'email': contact[-1].replace("[", "").replace("]", "").replace("'", ""),
+            'company': company,
+            'address': None,
+            'phone': numbers
+        }
     return(contact)
+
 
 my_file = 'test_email.eml'
 
 headers = header_parser(my_file)
 
-from_key = ['From'] # The key(s) you want
+from_key = ['From']  # The key(s) you want
 
 from_hdr = dict((k, headers[k]) for k in from_key if k in headers)
 from_val = str([*from_hdr.values()])
