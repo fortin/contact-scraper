b356253f968e6ee3b57838faca9d9099f0836c4d
fortin <fortin@users.noreply.github.com>
2019-05-25 19:21:32 +0200

    CountVectorizer exports to csv

--- countvectorizer-test.py
+++ countvectorizer-test.py
@@ -1,11 +1,16 @@
 import re
 import os
 import sys
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+import csv
 
 path = "/Users/antonio/pCloud Drive/My Documents/Linguistics/corpora/enron-email/maildir/arnold-j/sent"
 
 cv = CountVectorizer(lowercase=True)
+tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+tfidf_transformer.fit(word_count_vector)
 
 def read_files(path):
     # TODO skip headers?
@@ -21,8 +26,25 @@ def read_files(path):
                     train_set.append(data_set)
     return train_set
 
+# def build_data_frame(path, classification):
+#     rows = []
+#     index = []
+#     for file_name, text in read_files(path):
+#         rows.append({'text': text, 'class': classification})
+#         index.append(file_name)
+#
+#     data_frame = DataFrame(rows, index=index)
+#     return data_frame
+
+
 corpus = read_files(path)
+
 X = cv.fit_transform(corpus)
 
-print(cv.get_feature_names())
-print(X.toarray())
+feature_names = cv.get_feature_names()
+vectors = X.toarray()
+
+
+
+#
+# pd.DataFrame(vectors).to_csv("foo.csv")
