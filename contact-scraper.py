22c081be74a0097614e915daab023c7453ce8aff
fortin <fortin@users.noreply.github.com>
2019-09-23 22:13:31 +0200

    feat: dict_pop function matches from names-dataset
    Refactored string cleanup (additive)

--- contact-scraper.py
+++ contact-scraper.py
@@ -1,116 +1,117 @@
 #!/usr/bin/env python3
 #
-#  Contact Scraper for Gmail
+#  Contact Scraper for email
 #
-#  Created by Antonio Fortin on 2019-04-01.
 #  Copyright (c) 2019 Antonio Fortin. All rights reserved.
 #
 
 import re
-import email
-import email.parser
-import json
+from email.parser import HeaderParser
 import phonenumbers
+from names_dataset import NameDataset
 
 def header_parser(filename):
     # parse email headers
     with open(filename, 'r') as msg:
         my_email = msg.read()
-        parser = email.parser.HeaderParser()
-        raw_headers = parser.parsestr(my_email)
-    return(raw_headers)
-
-def header_cleaner(raw_headers):
-    str_headers = []
-    for h in raw_headers.items():
-        # stop when binary data starts
-        if h == "Content-Type: image" or h == "Content-Type: application":
-            break
-        else:
-            str_headers.append(h)
-    headers = str(str_headers)
-    # strip double quotes, convert brackets [()] to braces and make string dict-like for json.loads
-    headers = headers.replace('"', '').replace("[(", "{", 1).replace(")]", "}").replace("',", "':").replace("'", '"').replace("), (", ",")
-    # get rid of everything after the headers
-    headers = re.sub(r"{(.+?}).+", "\\1", headers)
-    # for char in headers:
-    #     headers = headers.translate({ord(i):None for i in '[]'})
+        parser = HeaderParser()
+        h = parser.parsestr(my_email)
+        headers = dict(h.items())
     return(headers)
 
-# TODO is there a less bruteforce way of doing the cleanup above?
 
-def number_catcher(filename):
+def num_catcher(filename):
     # extract all US phone numbers from message
     with open(filename, 'r') as msg:
         my_email = msg.read()
         numbers = set()
         for match in phonenumbers.PhoneNumberMatcher(my_email, "US"):
-            numbers.add(phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164))
-        if numbers == set():
+            numbers.add(phonenumbers.format_number(
+                match.number, phonenumbers.PhoneNumberFormat.E164))
+            if numbers != set():  # stop after first match (not ideal)
+                break
+        if numbers == set() or len(numbers) == 1:
             numbers = None
-        numbers = str(numbers).replace("{'", "").replace("'}", "")
+            return(numbers)
+        else:
+            numbers = numbers[-1]#str(numbers).replace("{'", "").replace("'}", "")
     return(numbers)
 
+
 def email_catcher(filename):
-    # extract all email addresses from message
+    # extract all email addresses from message (not necessary yet)
     pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)")
     emails = set()
     with open(filename) as f:
         for i, line in enumerate(f):
             for match in re.finditer(pattern, line):
                 emails.update(match.groups())
-# TODO check if filename is a file or string type and react accordingly.
     return(emails)
 
+
 def dict_pop(contact, numbers):
-    if contact[0].isupper():
+    """
+    Populate dictionary with contact info from headers.
+
+    Arguments:
+    contact - cleaned-up From header comprising sender's name and email address
+    numbers - phone numbers scraped from body
+
+    Outputs:
+    Python dictionary with contact fields as keys and scraped info as values
+    """
+
+    last_name = [ x for x in contact if m.search_last_name(x) ]
+    first_name = [ x for x in contact if m.search_first_name(x) ]
+    # catch cases where both names are valid last_names
+    if len(last_name) > 1:
+        # catch cases where both names are valid first_names
+        if len(first_name) > 1:
+            # assign variables according to 'first_name last_name' order in header
+            last_name = last_name[1]
+            first_name = first_name[0]
+        else:
+            # if only last_name is ambiguous, get it from the second list element
+            last_name = last_name[1]
+    # if only first_name is ambiguous, get it from first list element
+    elif len(first_name) > 1:
+        first_name = first_name[0]
+    # strip non-alphanumeric characters and convert lists to strings
+    last_name = re.sub(r'[^@. a-zA-Z]', '', str(last_name))
+    first_name = re.sub(r'[^@. a-zA-Z]', '', str(first_name))
+    email = [ x for x in contact if "@" in x ]
+    email = re.sub(r'[^@. a-zA-Z]', '', str(email))
+    domain = re.sub(r"[a-zA-Z0-9_.+-]+@([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",
+                    r"\1",
+                    email)
+    # TODO is there a way to have a for loop that applies the regex to each variable as a string, and overwrites the variable with the output?
+    try:
         contact = {
-                    'lastname' : contact[0].replace("[", "").replace("]", "").replace("'", ""),
-                    'firstname': contact[1].replace("[", "").replace("]", "").replace("'", ""),
-                    'email'    : contact[-1].replace("[", "").replace("]", "").replace("'", ""),
-                    'company'  : None,
-                    'address'  : None,
-                    'phone'    : numbers
-                    }
-    elif len(contact) < 4:
-        try:
-            contact = {
-                        'lastname' : contact[1].replace("[", "").replace("]", "").replace("'", ""),
-                        'firstname': contact[0].replace("[", "").replace("]", "").replace("'", ""),
-                        'email'    : contact[-1].replace("[", "").replace("]", "").replace("'", ""),
-                        'company'  : None,
-                        'address'  : None,
-                        'phone'    : numbers
-                        }
-        except:
-            pass
-    else:
-        company = str(contact[0:-1]).replace("[", "").replace("]", "").replace("'", "").replace('"', "").replace(",", "")
-        contact = {
-                    'lastname' : None,
-                    'firstname': None,
-                    'email'    : contact[-1].replace("[", "").replace("]", "").replace("'", ""),
-                    'company'  : company,
-                    'address'  : None,
-                    'phone'    : numbers
-                    }
+            'lastname': last_name,
+            'firstname': first_name,
+            'email': email,
+            'company': domain,
+            'address': None,
+            'phone': numbers
+        }
+    except:
+        pass
     return(contact)
 
+m = NameDataset()
 my_file = 'test_email.eml'
 
-raw_headers = header_parser(my_file)
-headers = header_cleaner(raw_headers)
-headers = json.loads(headers)
-from_key = ['From'] # The key(s) you want
+headers = header_parser(my_file)
 
-from_hdr = dict((k, headers[k]) for k in from_key if k in headers)
-from_val = str([*from_hdr.values()])
+from_val = headers['From']
+contact = re.sub(r'[^@. a-zA-Z]', '', from_val).split()
 
-contact = from_val.split()
-numbers = number_catcher(my_file)
+numbers = num_catcher(my_file)
+# print(numbers)
+contact = dict_pop(contact, numbers)
+
+# print(first_name, last_name)
+print(contact)
 
 # emails = email_catcher(my_file)
 # print(emails)
-contact = dict_pop(contact, numbers)
-
-print(contact)
