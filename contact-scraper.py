c7861eec8f5e286206e943dad4a3296a320980e1
fortin <fortin@users.noreply.github.com>
2019-10-03 22:48:50 +0200

    Squashed commit of the following:
    commit 6f4b1283e8b3dc325bea8c676ebb9d6b457f86ff
    Author: fortin <fortin@users.noreply.github.com>
    Date:   Thu Oct 3 22:01:07 2019 +0200
    
        feat: all names cases working
    
        also refactored: email regex compiled
    
    commit 9488db15ed705324ed9611c347b4e5c5b7d19a99
    Author: fortin <fortin@users.noreply.github.com>
    Date:   Sun Sep 29 11:44:42 2019 +0200
    
        docs: added docstrings and comments
    
    commit 947d031f3dcb108adddffead3c411d41ec061b3d
    Author: fortin <fortin@users.noreply.github.com>
    Date:   Sun Sep 29 11:44:42 2019 +0200
    
        docs: added docstrings and comments
    
    commit e5b157e0c91dbedfa7462fdc05a282196a9ed9e5
    Author: fortin <fortin@users.noreply.github.com>
    Date:   Fri Sep 27 12:12:15 2019 +0200
    
        feat: added body extractor and body html stripper
    
    commit 021e5514c02fb06125cc1616c7bcb535738ec8ba
    Author: fortin <fortin@users.noreply.github.com>
    Date:   Thu Sep 26 21:08:28 2019 +0200
    
        feat: names and phone numbers matched
    
        dict_pop populated correctly
    
    commit 22c081be74a0097614e915daab023c7453ce8aff
    Author: fortin <fortin@users.noreply.github.com>
    Date:   Mon Sep 23 22:13:31 2019 +0200
    
        feat: dict_pop function matches from names-dataset
    
        Refactored string cleanup (additive)
    
    commit af0e2e3a94bd9f9d53e35e408865adf49176e74c
    Author: fortin <fortin@users.noreply.github.com>
    Date:   Wed May 1 18:26:35 2019 +0200
    
        feat: contact dictionary populated with names, email, and a phone number if present
    
        address and company keys left as None for now
    
    commit 82a9185cc0c63f0b36446e2c0446ee8d7a10a66d
    Author: fortin <fortin@users.noreply.github.com>
    Date:   Tue Apr 30 18:00:13 2019 +0200
    
        feat: populating python dictionary
    
        extracted relevant info from From: header
        TODO: if there are more than (say) 3 elements in From, ask whether it's a company rather than a personal name

--- contact-scraper.py
+++ contact-scraper.py
@@ -5,7 +5,9 @@
 #  Copyright (c) 2019 Antonio Fortin. All rights reserved.
 #
 
+import sys
 import re
+import email
 from email.parser import HeaderParser
 import phonenumbers
 from names_dataset import NameDataset
@@ -19,25 +21,57 @@ def header_parser(filename):
         headers = dict(h.items())
     return(headers)
 
+def body_extractor(filename):
+    """
+    Extract body of email message from file.
+    """
+    msg = email.message_from_file(open(filename, 'r'))
+    body = ''
+
+    if msg.is_multipart():
+        for part in msg.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get('Content-Disposition'))
+
+        # skip any text/plain (txt) attachments
+            if ctype == 'text/plain' or ctype == 'text/html' and 'attachment' not in cdispo:
+                body = part.get_payload(decode=True) # decode
+                break
+    # not multipart - i.e. plain text, no attachments
+    else:
+        body = b.get_payload(decode=True)
+    # strip HTML and remove \r, \t, \n
+    body = re.sub('<[^<]+?>', '', str(body)).replace('\\r', '\r').replace('\\n', '\n').replace('\\t', '')
+    # is there a more general, additive way to do this?
+    return(body)
 
 def num_catcher(filename):
-    # extract all US phone numbers from message
+    """
+    Extract all US phone numbers from message and return last one
+    """
     with open(filename, 'r') as msg:
         my_email = msg.read()
         numbers = set()
         for match in phonenumbers.PhoneNumberMatcher(my_email, "US"):
             numbers.add(phonenumbers.format_number(
                 match.number, phonenumbers.PhoneNumberFormat.E164))
-            if numbers != set():  # stop after first match (not ideal)
-                break
-        if numbers == set():
+            # if numbers != set():  # stop after first match (not ideal)
+            #     break
+        if len(numbers) == 0:
             numbers = None
-        numbers = str(numbers).replace("{'", "").replace("'}", "")
+            return(numbers)
+        # else:
+        #     numbers = numbers[0]#str(numbers).replace("{'", "").replace("'}", "")
+    numbers = list(numbers)
+    numbers = numbers[-1]
     return(numbers)
 
 
 def email_catcher(filename):
-    # extract all email addresses from message (not necessary yet)
+    """
+    Extract all email addresses from message
+    Not used and probably not necessary
+    """
     pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)")
     emails = set()
     with open(filename) as f:
@@ -47,24 +81,52 @@ def email_catcher(filename):
     return(emails)
 
 
-def dict_pop(contact, numbers):
-    domain = re.sub(r"[a-zA-Z0-9_.+-]+@([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",
-                    r"\1",
-                    contact[-1])
+def dict_pplt(contact, numbers):
+    email_regex = re.compile(r"(^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$))")
+    last_name = [ x for x in contact if m.search_last_name(x) ]
+    first_name = [ x for x in contact if m.search_first_name(x) ]
+    # catch cases where both names are valid last_names
+    if len(last_name) > 1:
+        # catch cases where both names are valid first_names
+        if len(first_name) > 1:
+            # assign variables according to 'first_name last_name' order in header
+            last_name = last_name[1]
+            first_name = first_name[0]
+        else:
+            # if only last_name is ambiguous, get it from the second list element
+            last_name = last_name[1]
+    # if only first_name is ambiguous, get it from first list element
+    elif len(first_name) > 1:
+        first_name = first_name[0]
+    elif (len(last_name) == 1) & (len(first_name) == 0):
+        last_name = last_name[0]
+    elif (len(last_name) == 0) & (len(first_name) == 1):
+        first_name = first_name[0]
+    else:
+        pass
+    # strip non-alphanumeric characters and convert lists to strings
+    last_name = re.sub(r'[^@. a-zA-Z]', '', str(last_name))
+    first_name = re.sub(r'[^@. a-zA-Z]', '', str(first_name))
+    try:
+        email = [ x for x in contact if bool(re.search(email_regex, x)) ][-1]
+    except:
+        email = None
+    email = re.sub(r'[^@. a-zA-Z]', '', str(email))
+    domain = re.sub(email_regex, r"\2", email)
     try:
         contact = {
-            'lastname': contact[1],
-            'firstname': contact[0],
-            'email': contact[-1],
+            'lastname': last_name,
+            'firstname': first_name,
+            'email': email,
             'company': domain,
             'address': None,
             'phone': numbers
-        }
+            }
     except:
         pass
     return(contact)
 
-
+m = NameDataset()
 my_file = 'test_email.eml'
 
 headers = header_parser(my_file)
@@ -73,9 +135,16 @@ from_val = headers['From']
 contact = re.sub(r'[^@. a-zA-Z]', '', from_val).split()
 
 numbers = num_catcher(my_file)
-contact = dict_pop(contact, numbers)
+# print(numbers)
+contact_dict = dict_pplt(contact, numbers)
 
-print(contact)
+print(contact_dict)
+
+body = body_extractor(my_file)
+print(body)
+
+# name = (contact[0] + ' ' + contact[1])
+# print(name)
 
 # emails = email_catcher(my_file)
 # print(emails)
